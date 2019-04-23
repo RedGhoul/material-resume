a1397a203c615c21a27353d75d7eaf72a1f731b1
RedGhoul <avaneesab5@gmail.com>
2019-04-22 22:18:42 -0400

    updated resume

--- src/resume.js
+++ src/resume.js
@@ -1,96 +1,104 @@
-angular.module('Resume', ['ngMaterial'])
+angular
+  .module("Resume", ["ngMaterial"])
 
-.component('resume', {
-  transclude: true,
-  template: `<div class="resume">
+  .component("resume", {
+    transclude: true,
+    template: `<div class="resume">
     <section class="main" ng-transclude></section>
   </div>`
-})
+  })
 
-.component('resumeHeading', {
-  controllerAs: 'r',
-  transclude: true,
-  bindings: {
-    firstName: '@',
-    lastName: '@',
-    website: '@',
-    phone: '@',
-    email: '@',
-    github: '@',
-    linkedin: '@'
-  },
-  template: `<div class="header">
+  .component("resumeHeading", {
+    controllerAs: "r",
+    transclude: true,
+    bindings: {
+      firstName: "@",
+      lastName: "@",
+      website: "@",
+      phone: "@",
+      email: "@",
+      github: "@",
+      linkedin: "@"
+    },
+    template: `<div class="header">
     <div class="page-title-container">
       <h1 class="page-title page-title-first">{{r.firstName}}</h1>
       <h1 class="page-title page-title-last">{{r.lastName}}</h1>
+      <div class="container">
+        <div class="row">
+          <a ng-if="r.email" class="md-subhead page-subhead" ng-href="mailto:{{r.email}}" target="_blank">
+            {{r.email}}
+          </a> | 
+          <span ng-if="r.phone" class="md-subhead page-subhead">
+            {{r.phone}}
+          </span>
+        <div>
+      
+      </div>
+      
     </div>
+    <hr/>
     <div class="header-icons">
-      <a ng-if="r.website" class="md-subhead page-subhead" ng-href="https://{{r.website}}" target="_blank">
-        <i class="fa fa-lg fa-code"></i>{{r.website}}
-      </a>
-      <span ng-if="r.phone" class="md-subhead page-subhead">
-        <i class="fa fa-lg fa-phone"></i>{{r.phone}}
-      </span>
-      <a ng-if="r.email" class="md-subhead page-subhead" ng-href="mailto:{{r.email}}" target="_blank">
-        <i class="fa fa-lg fa-envelope"></i>{{r.email}}
-      </a>
+      <a ng-if="r.website" class="md-subhead page-subhead" ng-href="https://www.{{r.website}}" target="_blank">
+        https://www.{{r.website}}
+      </a>|
       <a ng-if="r.github" class="md-subhead page-subhead" ng-href="https://github.com/{{r.github}}" target="_blank">
-        <i class="fa fa-lg fa-github"></i>{{r.github}}
-      </a>
-      <a ng-if="r.linkedin" class="md-subhead page-subhead" ng-href="www.linkedin.com/in/avaneesa" target="_blank">
-        <i class="fa fa-lg fa-linkedin-square"></i>{{r.linkedin}}
+        https://github.com/{{r.github}}
+      </a>|
+      <a ng-if="r.linkedin" class="md-subhead page-subhead" ng-href="https://www.linkedin.com/in/avaneesa" target="_blank">
+        https://linkedin.com/in/{{r.linkedin}}
       </a>
     </div>
   </div>
   <ng-transclude></ng-transclude>`
-})
+  })
 
-.component('resumeBody', {
-  transclude: true,
-  template: `<div layout="row" ng-transclude></div>`
-})
+  .component("resumeBody", {
+    transclude: true,
+    template: `<div layout="row" ng-transclude></div>`
+  })
 
-.component('resumeColumn', {
-  transclude: true,
-  template: `<div ng-transclude></div>`
-})
+  .component("resumeColumn", {
+    transclude: true,
+    template: `<div ng-transclude></div>`
+  })
 
-.component('sidebarCategory', {
-  controllerAs: 'r',
-  bindings: {
-    name: '@'
-  },
-  transclude: true,
-  template: `<div flex="" class="category-row">
+  .component("sidebarCategory", {
+    controllerAs: "r",
+    bindings: {
+      name: "@"
+    },
+    transclude: true,
+    template: `<div flex="" class="category-row">
     <div class="category">
       <h5 class="category-row-title">{{r.name}}</h5>
       <ng-transclude></ng-transclude>
     </div>
   </div>`
-})
+  })
 
-.component('category', {
-  controllerAs: 'r',
-  bindings: {
-    name: '@'
-  },
-  transclude: true,
-  template: `<div flex="" class="category-row">
+  .component("category", {
+    controllerAs: "r",
+    bindings: {
+      name: "@"
+    },
+    transclude: true,
+    template: `<div flex="" class="category-row">
     <h4 class="category-row-title">{{r.name}}</h4>
     <ng-transclude></ng-transclude>
   </div>`
-})
+  })
 
-.component('categoryItem', {
-  controllerAs: 'r',
-  bindings: {
-    title: '@',
-    subtitle: '@'
-  },
-  transclude: true,
-  template: `<div class="category">
+  .component("categoryItem", {
+    controllerAs: "r",
+    bindings: {
+      title: "@",
+      subtitle: "@"
+    },
+    transclude: true,
+    template: `<div class="category">
     <h5 class="category-title md-body-2">{{r.title}}</h5>&nbsp;
     <h5 class="category-subtitle md-caption">{{r.subtitle}}</h5>
     <ul class="category-bullets" ng-transclude></ul>
   </div>`
-})
+  });
